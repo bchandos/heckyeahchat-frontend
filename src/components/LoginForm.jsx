aeb2740925b22178d135d3a393eacdc4b834198e
Bill <bill.chandos@gmail.com>
2020-12-21 17:20:20 +0000

    implement api calls on form submit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { login } from '../api/api';
 import SubmitButton from './SubmitButton';
 import TextInput from './TextInput';
 
 
 class LoginForm extends React.Component {
+  submitLogin = async (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    let jwt = await login(form.elements.username.value, form.elements.password.value);
+    localStorage.setItem('jwt', jwt);
+    // somehow redirect here
+  }
   render() {
     return (
       <div className="w-full max-w-xs m-auto bg-gray-100 rounded p-5">
@@ -11,14 +20,16 @@ class LoginForm extends React.Component {
           <i className="material-icons text-4xl">face</i>
           <h1 className="font-bold text-2xl tracking-wider">Login</h1>
         </header>
-        <form onSubmit={(e) => e.preventDefault()}>
+        <form onSubmit={this.submitLogin}>
           <TextInput name="username" labelText="Email" />
           <TextInput name="password" labelText="Password" />
           <div><SubmitButton buttonText="Log In" /></div>
         </form>
         <footer>
           <a class="text-gray-700 hover:text-pink-700 text-sm float-left" href="/password-recovery">Forgot Password?</a>
-          <a class="text-gray-700 hover:text-pink-700 text-sm float-right" href="/register">Create Account</a>
+          <Link to='/register'>
+            <span class="text-gray-700 hover:text-pink-700 text-sm float-right">Create Account</span>
+          </Link>
         </footer>
       </div>
     )
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { registerAccount } from '../api/api';
 import SubmitButton from './SubmitButton';
 import TextInput from './TextInput';
+import { withRouter } from 'react-router-dom';
 
 class RegisterForm extends React.Component {
+  submitRegister = async (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    let jwt = await registerAccount(form.elements.username.value, form.elements.password.value);
+    localStorage.setItem('jwt', jwt);
+    // somehow redirect here
+    this.props.history.push('/');
+  }
   render() {
     return (
       <div className="w-full max-w-xs m-auto bg-gray-100 rounded p-5">
@@ -10,11 +20,11 @@ class RegisterForm extends React.Component {
           <i className="material-icons text-4xl text-gray-300">face</i>
           <h1 className="font-bold text-2xl tracking-wider">Register</h1>
         </header>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <TextInput name="username" labelText="Email" />
-          <TextInput name="password" labelText="Password" />
-          <TextInput name="confirm-password" labelText="Confirm Password" />
-          <div><SubmitButton buttonText="Register" /></div>
+        <form onSubmit={this.submitRegister}>
+          <TextInput name="username" type="text" labelText="Email" />
+          <TextInput name="password" type="password" labelText="Password" />
+          <TextInput name="confirm-password" type="password" labelText="Confirm Password" />
+          <SubmitButton buttonText="Register" disabled={false} />
         </form>
         <footer>
           <a class="text-gray-700 hover:text-pink-700 text-sm" href="/password">Forgot Password?</a>
@@ -24,4 +34,4 @@ class RegisterForm extends React.Component {
   }
 }
 
-export default RegisterForm;
+export default withRouter(RegisterForm);
