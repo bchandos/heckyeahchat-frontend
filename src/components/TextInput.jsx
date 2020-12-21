164e39ec65cb2512d231c3fd7d4ec66b26429328
Bill <bill.chandos@gmail.com>
2020-12-21 17:16:54 +0000

    -Only animate label if value is empty -Allow input type as prop

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -4,10 +4,10 @@ class TextInput extends React.Component {
 
   animateLabel = (e) => {
     const label = e.currentTarget.previousElementSibling;
-    if (e.type === 'focus') {
+    if (e.type === 'focus' && !e.currentTarget.value) {
       label.classList.add('text-xs', 'top-0', 'text-gray-500');
       label.classList.remove('text-base', 'top-2', 'text-gray-300');
-    } else if (e.type === 'blur') {
+    } else if (e.type === 'blur' && !e.currentTarget.value) {
       label.classList.add('text-base', 'top-2', 'text-gray-300');
       label.classList.remove('text-xs', 'top-0', 'text-gray-500');
     }
@@ -19,7 +19,7 @@ class TextInput extends React.Component {
         <label className="absolute left-1 top-2 block text-gray-300 transition-label" htmlfor={this.props.name}>{this.props.labelText}</label>
         <input
           className="w-full p-1 text-gray-700 border-b-2 border-gray-500 outline-none"
-          type="text"
+          type={this.props.type}
           name={this.props.name}
           onFocus={this.animateLabel}
           onBlur={this.animateLabel}
