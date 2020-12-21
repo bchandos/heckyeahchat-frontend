b023a36800468477daef1ec33dbcc7aea1f8516a
Bill <bill.chandos@gmail.com>
2020-12-21 17:17:54 +0000

    implement react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import BaseNav from './components/BaseNav';
 import Footer from './components/Footer';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Chat from './components/Chat';
 
 function App() {
   return (
-    <div className="flex flex-col h-screen bg-gray-700">
-      <BaseNav />
-      {/* <RegisterForm /> */}
-      <LoginForm />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col h-screen bg-gray-700">
+        <BaseNav />
+        <Switch>
+          <Route path='/register'>
+            <RegisterForm />
+            <Footer />
+          </Route>
+          <Route path='/chat'>
+            <Chat />
+          </Route>
+          <Route path='/'>
+            <LoginForm />
+            <Footer />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
