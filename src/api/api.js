ed389fc19549a3faeb9fa35ab45dc204414794a7
Bill <bill.chandos@gmail.com>
2020-12-22 01:39:36 +0000

    more API calls

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,5 @@
+// Authorization Routes
+
 export const registerAccount = async (username, password) => {
   const response = await fetch('http://localhost:3333/auth/register', {
     method: 'POST',
@@ -28,4 +30,28 @@ export const login = async (username, password) => {
 
   const json = await response.json();
   return json.jwt;
+}
+
+// Conversations
+
+export const getConversations = async (userId) => {
+  const jwt = localStorage.getItem('jwt');
+  const response = await fetch(`http://localhost:3333/user/${userId}/conversations`, {
+    headers: {
+      'Authorization': `Bearer ${jwt}`
+    }
+  });
+  return response.json();
+}
+
+// Messages
+
+export const getMessages = async (conversationId) => {
+  const jwt = localStorage.getItem('jwt');
+  const response = await fetch(`http://localhost:3333/conversation/${conversationId}/messages`, {
+    headers: {
+      'Authorization': `Bearer ${jwt}`
+    }
+  });
+  return response.json();
 }
